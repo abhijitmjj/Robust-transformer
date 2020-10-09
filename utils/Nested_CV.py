22521bae55bf27b7c8d6f0b1fc01d5f97f7aebc6
Abhijit Gupta <abhijit038@gmail.com>
2020-10-09 12:34:49 +0530

    updated alpha

--- utils/Nested_CV.py
+++ utils/Nested_CV.py
@@ -38,3 +38,29 @@ def inner_split(*, key: jax.numpy.lax_numpy.ndarray,
         mask[np.arange(j*mark, (j+1)*mark)] = True
         yield {"Train": train_data.iloc[permutation[~mask], :],
                "Val": train_data.iloc[permutation[mask], :]}
+        
+def outer_loop( *, key: jax.numpy.lax_numpy.ndarray,
+                  raw_data: pd.DataFrame, n: int) -> Dict[str, pd.DataFrame]:
+    """Yields a dictionary of n fold splits"""
+
+    permutation = random.permutation(key, jnp.arange(len(raw_data)))
+    mark = len(raw_data)//n
+    for i in range(n):
+        mask = np.zeros(len(raw_data), dtype=bool)
+        mask[np.arange(i*mark, (i+1)*mark)] = True
+        yield {"key": key, "outer_fold_data":{"Test": raw_data.iloc[permutation[mask], :],
+               "Train": raw_data.iloc[permutation[~mask], :]}, "n": n}
+
+        
+def inner_loop( *, key: jax.numpy.lax_numpy.ndarray,
+                outer_fold_data: Dict[str, pd.DataFrame], n: int) -> Dict[str, pd.DataFrame]:
+    """Yields inner split for parameter tuning"""
+    train_data: pd.DataFrame = outer_fold_data["Train"]
+    permutation = random.permutation(key, jnp.arange(len(train_data)))
+    mark = len(train_data)//n
+    for j in range(n):
+        mask = np.zeros(len(train_data), dtype=bool)
+        mask[np.arange(j*mark, (j+1)*mark)] = True
+        yield {"Train": train_data.iloc[permutation[~mask], :],
+               "Val": train_data.iloc[permutation[mask], :]}
+        
--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ import dill
 from .Read_Data import read_line, load_pkl
 from .Nested_CV import split_shuffle as outer_split
 from .Nested_CV import inner_split
+from .Nested_CV import inner_loop, outer_loop
